28208f908a645458ef983da1fa146a6e566f34b4
mdjorup <michael.djorup@gmail.com>
2022-01-14 10:56:21 -0500

    Finished ui for login and register pages

--- src/components/authui/Login.js
+++ src/components/authui/Login.js
@@ -1,10 +1,44 @@
-import React from 'react'
+import React, {useState} from 'react'
 import '../../css/Login.css';
+import {SiBetfair} from 'react-icons/si';
+import {IconContext} from 'react-icons';
+import { useNavigate } from 'react-router-dom';
 
 function Login() {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  let navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate('/');
+  }
+
+  const handleLogin = () => {
+    navigate('/')
+  }
+
+
   return (
-    <div>
-      <h1>Login</h1>
+    <div className='login'>
+      <div className='login__header'>
+        <div className='login__logo' onClick={navigateHome}>
+          <div className="wager__icon">
+            <IconContext.Provider value={{ color: "aqua", size: '2.5em'}}>
+              <SiBetfair/>
+            </IconContext.Provider>
+          </div>
+          <h2>Blitz</h2>
+        </div> 
+      </div> 
+      <div className="login__body">
+        <input type='email' placeholder='email' onChange={event => setEmail(event.target.value)}></input>
+        <input type='password' placeholder='password' onChange={event => setPassword(event.target.value)}></input>
+        <button className='login__button' onClick={handleLogin}>
+          Log In
+        </button>
+      </div>
     </div>
   )
 }
--- src/components/authui/Register.js
+++ src/components/authui/Register.js
@@ -1,16 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom';
 import '../../css/Register.css';
+import {SiBetfair} from 'react-icons/si';
+import {IconContext} from 'react-icons';
 
 function Register() {
 
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  let navigate = useNavigate();
+
+  const navigateHome = () => {
+    navigate('/');
+  }
+
+  const handleRegister = () => {
+    navigate('/')
+  }
+
   return (
     <div className='register'>
+      <div className='register__header'>
+        <div className='register__logo' onClick={navigateHome}>
+          <div className="wager__icon">
+            <IconContext.Provider value={{ color: "aqua", size: '2.5em'}}>
+              <SiBetfair/>
+            </IconContext.Provider>
+          </div>
+          <h2>Blitz</h2>
+        </div> 
+      </div> 
       <div className="register__body">
-        <h1>Register</h1>
-        <input type='text' placeholder='First name'></input>
-        <input type='text' placeholder='Last name'></input>
-        <input type='email' placeholder='email'></input>
-        <input type='password' placeholder='password'></input>
+        <input type='text' placeholder='First name' onChange={event => setFirstName(event.target.value)}></input>
+        <input type='text' placeholder='Last name' onChange={event => setLastName(event.target.value)}></input>
+        <input type='email' placeholder='email' onChange={event => setEmail(event.target.value)}></input>
+        <input type='password' placeholder='password' onChange={event => setPassword(event.target.value)}></input>
+        <button className='register__button' onClick={handleRegister}>
+          Register
+        </button>
       </div>
     </div>
   )
